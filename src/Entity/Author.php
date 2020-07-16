1ae690900532bf549e146f0f2b27a41d4996bfda
Lucas-Alarcon <lucas.alarcon@etu.univ-amu.fr>
2020-07-16 11:24:50 +0200

    crud MeetUp

--- src/Entity/Author.php
+++ src/Entity/Author.php
@@ -57,4 +57,8 @@ class Author
 
         return $this;
     }
+
+    public function __toString() {
+        return $this->firstName . ' ' . $this->lastName;
+    }
 }
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -135,4 +135,8 @@ class User
 
         return $this;
     }
+
+    public function __toString() {
+        return $this->firstName . ' ' . $this->lastName;
+    }
 }
