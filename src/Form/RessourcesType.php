9d05bb5b09f0070d30890256b18bbbf8ea2c5e3b
Matthias-dmt <matthiasdumont.pro@gmail.com>
2020-07-16 11:44:07 +0200

    crud resource

---
+++ src/Form/RessourcesType.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Ressources;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Choice;
+
+class RessourcesType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('url')
+            ->add('type', ChoiceType::class, [
+                'choices' => [
+                    'article' => 'article_type',
+                    'video' => 'video_type',
+                    'movie' => 'movie_type'
+                ],
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Ressources::class,
+        ]);
+    }
+}
