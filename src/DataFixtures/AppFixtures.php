98f8e17c1241aea06e85e0eb92e6648bb75f0e43
lusK13 <lucasbelsunce@gmail.com>
2020-07-20 17:34:11 +0200

    Revert "faker commit"
    This reverts commit d83f8865d6e2dc7a0acb2f18939d79835cd39077, reversing
    changes made to 01f8309b75fb5982b9089ec800b5fd02cda065af.

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -21,10 +21,7 @@ use App\Entity\Maintenance;
 use App\Entity\Participates;
 use App\Entity\IsInvolvedIn;
 use App\Entity\MeetUp;
-<<<<<<< HEAD
 
-=======
->>>>>>> 49544e6b6c46fb3417beb5114098575cecd2a1c7
 use Faker;
 
 class AppFixtures extends Fixture
@@ -36,7 +33,7 @@ class AppFixtures extends Fixture
         $this->manager = $entityManager;
     }
 
-    public function load(ObjectManager $manager)
+    public function load(ObjectManager $manager) 
     {
         // $product = new Product();
         // $manager->persist($product);
@@ -49,37 +46,37 @@ class AppFixtures extends Fixture
         $authorRep = $this->manager->getRepository(Author::class);
         $employeeRep = $this->manager->getRepository(Employee::class);
         $memberRep = $this->manager->getRepository(Member::class);
-        
+        var_dump($employeeRep->findByAll());
         // on créé 100 book
         for ($i = 0; $i < 100; $i++) {
             $book = new Book();
             $book->setTitle($faker->name);
             $book->setCote($faker->text($maxNbChars = 5));
-            $book->setFormat($faker->randomElement($array = array('de poche', 'grand', 'moyen')));
+            $book->setFormat($faker->randomElement($array = array ('de poche','grand','moyen')));
             $book->setCodeOeuvre($faker->randomNumber($nbDigits = NULL, $strict = false));
             $book->setPages($faker->numberBetween($min = 20, $max = 500));
             $manager->persist($book);
         }
         $manager->flush();
 
-        // on créé 100 ebook
-        for ($i = 0; $i < 100; $i++) {
+         // on créé 100 ebook
+         for ($i = 0; $i < 100; $i++) {
             $ebook = new EBook();
             $ebook->setTitle($faker->name);
             $ebook->setCote($faker->text($maxNbChars = 5));
-            $ebook->setFormat($faker->randomElement($array = array('de poche', 'grand', 'moyen')));
+            $ebook->setFormat($faker->randomElement($array = array ('de poche','grand','moyen')));
             $ebook->setCodeOeuvre($faker->randomNumber($nbDigits = NULL, $strict = false));
             $ebook->setPages($faker->numberBetween($min = 20, $max = 500));
             $manager->persist($ebook);
         }
         $manager->flush();
 
-        // on créé 100 cd
-        for ($i = 0; $i < 100; $i++) {
+         // on créé 100 cd
+         for ($i = 0; $i < 100; $i++) {
             $cd = new CD();
             $cd->setTitle($faker->name);
             $cd->setCote($faker->text($maxNbChars = 5));
-            $cd->setFormat($faker->randomElement($array = array('audio', 'video', 'blueray')));
+            $cd->setFormat($faker->randomElement($array = array ('audio','video','blueray')));
             $cd->setCodeOeuvre($faker->randomNumber($nbDigits = NULL, $strict = false));
             $cd->setPlages($faker->numberBetween($min = 1, $max = 100));
             $cd->setDuration($faker->dateTime($max = 'now', $timezone = null));
@@ -87,35 +84,35 @@ class AppFixtures extends Fixture
         }
         $manager->flush();
 
-        // on créé 100 journal
-        for ($i = 0; $i < 100; $i++) {
+         // on créé 100 journal
+         for ($i = 0; $i < 100; $i++) {
             $journal = new Journal();
             $journal->setTitle($faker->name);
             $journal->setCote($faker->text($maxNbChars = 5));
-            $journal->setFormat($faker->randomElement($array = array('de poche', 'grand', 'moyen')));
+            $journal->setFormat($faker->randomElement($array = array ('de poche','grand','moyen')));
             $journal->setCodeOeuvre($faker->randomNumber($nbDigits = NULL, $strict = false));
-            $journal->setPeriodicity($faker->randomElement($array = array('1 jour', '1 semaine', '1 mois')));
+            $journal->setPeriodicity($faker->randomElement($array = array ('1 jour','1 semaine','1 mois')));
             $journal->setSubscriptionDate($faker->dateTime($max = 'now', $timezone = null));
             $manager->persist($journal);
         }
         $manager->flush();
 
-        // on créé 100 DVD
+         // on créé 100 DVD
         for ($i = 0; $i < 100; $i++) {
             $dvd = new DVD();
             $dvd->setTitle($faker->name);
             $dvd->setCote($faker->text($maxNbChars = 5));
-            $dvd->setFormat($faker->randomElement($array = array('audio', 'video', 'blueray')));
+            $dvd->setFormat($faker->randomElement($array = array ('audio','video','blueray')));
             $dvd->setCodeOeuvre($faker->randomNumber($nbDigits = NULL, $strict = false));
             $dvd->setDuration($faker->dateTime($max = 'now', $timezone = null));
             $manager->persist($dvd);
         }
         $manager->flush();
 
-        // on créé 100 author
+         // on créé 100 author
         for ($i = 0; $i < 100; $i++) {
             $author = new Author();
-            $author->setFirstName($faker->firstName($gender = 'male' | 'female'));
+            $author->setFirstName($faker->firstName($gender = 'male'|'female'));
             $author->setLastName($faker->lastName);
             $manager->persist($author);
         }
@@ -124,9 +121,9 @@ class AppFixtures extends Fixture
         // on créé 100 member
         for ($i = 0; $i < 100; $i++) {
             $member = new Member();
-            $member->setPseudo($faker->firstName($gender = 'male' | 'female') . $faker->lastName);
+            $member->setPseudo($faker->firstName($gender = 'male'|'female') . $faker->lastName);
             $member->setPassword($faker->password);
-            $member->setFirstName($faker->firstName($gender = 'male' | 'female'));
+            $member->setFirstName($faker->firstName($gender = 'male'|'female'));
             $member->setLastName($faker->lastName);
             $member->setPostalCode($faker->numberBetween($min = 10000, $max = 99999));
             $member->setCity($faker->city);
@@ -139,18 +136,18 @@ class AppFixtures extends Fixture
         // on créé 100 employee
         for ($i = 0; $i < 100; $i++) {
             $employee = new Employee();
-            $employee->setPseudo($faker->firstName($gender = 'male' | 'female') . $faker->lastName);
-            $employee->setFirstName($faker->firstName($gender = 'male' | 'female'));
+            $employee->setPseudo($faker->firstName($gender = 'male'|'female') . $faker->lastName);
+            $employee->setFirstName($faker->firstName($gender = 'male'|'female'));
             $employee->setLastName($faker->lastName);
             $employee->setPassword($faker->password);
             $manager->persist($employee);
         }
         $manager->flush();
 
-        // on créé 10 meet up
-        for ($i = 0; $i < 10; $i++) {
+        // on créé 100 meet up
+        for ($i = 0; $i < 100; $i++) {
             $meetUp = new MeetUp();
-            $meetUp->setEmployee($employeeRep->find($faker->numberBetween($min = 101, $max = 200)));
+            $meetUp->setEmployee($employeeRep->find($faker->numberBetween($min = 100, $max = 200)));
             $meetUp->setAuthor($authorRep->find($faker->numberBetween($min = 1, $max = 100)));
             $meetUp->setDate($faker->dateTimeBetween($startDate = 'now', $endDate = '1 years', $timezone = null));
             $manager->persist($meetUp);
@@ -161,7 +158,7 @@ class AppFixtures extends Fixture
         for ($i = 0; $i < 100; $i++) {
             $ressources = new Ressources();
             $ressources->setUrl($faker->url);
-            $ressources->setType($faker->randomElement($array = array('article', 'video', 'movie')));
+            $ressources->setType($faker->randomElement($array = array ('article','video','movie')));
             $ressources->setDocument($docRep->find($faker->numberBetween($min = 1, $max = 500)));
             $manager->persist($ressources);
         }
@@ -176,15 +173,13 @@ class AppFixtures extends Fixture
             $borrowing->setMember($memberRep->find($faker->numberBetween($min = 1, $max = 100)));
             $borrowing->setDocument($docRep->find($faker->numberBetween($min = 1, $max = 500)));
             $manager->persist($borrowing);
-        }
-        $manager->flush();
 
         // on créé 100 participates
         for ($i = 0; $i < 100; $i++) {
             $participates = new Participates();
             $participates->setPlaces($faker->numberBetween($min = 20, $max = 200));
-            $participates->setMeetUp($meetUpRep->find($faker->numberBetween($min = 1, $max = 10)));
-            $participates->setUser($userRep->find($faker->numberBetween($min = 1, $max = 100)));
+            $participates->setMeetUp($meetUpRep->find($faker->numberBetween($min = 0, $max = 100)));
+            $participates->setUser($userRep->find($faker->numberBetween($min = 0, $max = 100)));
             $manager->persist($participates);
         }
         $manager->flush();
@@ -192,27 +187,29 @@ class AppFixtures extends Fixture
         // on créé 100 IsInvolvdIn
         for ($i = 0; $i < 100; $i++) {
             $isInvolvedIn = new IsInvolvedIn();
-
-            $isInvolvedIn->setDocument($docRep->find($faker->numberBetween($min = 1, $max = 500)));
-
-            switch (get_class($isInvolvedIn->getDocument())) {
+            
+            $isInvolvedIn->setDocument($docRep->find($faker->numberBetween($min = 0, $max = 500)));
+            
+            switch(get_class($isInvolvedIn->getDocument())) 
+            {
                 case "App\Entity\DVD":
-                    $isInvolvedIn->setRole($faker->randomElement($array = array('acteur', 'producteur', 'scénariste', 'réalisateur')));
+                    $isInvolvedIn->setRole($faker->randomElement($array = array ('acteur','producteur','scénariste', 'réalisateur'))); 
                     break;
                 case "App\Entity\CD":
-                    $isInvolvedIn->setRole($faker->randomElement($array = array('chanteur', 'compositeur', 'musicien')));
+                    $isInvolvedIn->setRole($faker->randomElement($array = array ('chanteur','compositeur','musicien'))); 
                     break;
                 case "App\Entity\Journal":
-                    $isInvolvedIn->setRole($faker->randomElement($array = array('rédacteur', 'producteur')));
+                    $isInvolvedIn->setRole($faker->randomElement($array = array ('rédacteur','producteur'))); 
                     break;
                 case "App\Entity\Book":
-                    $isInvolvedIn->setRole($faker->randomElement($array = array('éditeur', 'illustrateur', 'auteur')));
+                    $isInvolvedIn->setRole($faker->randomElement($array = array ('éditeur','illustrateur','auteur'))); 
                     break;
                 case "App\Entity\EBook":
-                    $isInvolvedIn->setRole($faker->randomElement($array = array('narrateur', 'auteur', 'illustrateur')));
+                    $isInvolvedIn->setRole($faker->randomElement($array = array ('narrateur','auteur','illustrateur'))); 
                     break;
+
             }
-            $isInvolvedIn->setAuthor($authorRep->find($faker->numberBetween($min = 1, $max = 100)));
+            $isInvolvedIn->setAuthor($authorRep->find($faker->numberBetween($min = 0, $max = 100)));
             $manager->persist($isInvolvedIn);
         }
         $manager->flush();
@@ -220,13 +217,14 @@ class AppFixtures extends Fixture
         // on créé 100 maintenance
         for ($i = 0; $i < 100; $i++) {
             $maintenance = new Maintenance();
-            $maintenance->setStatus($faker->randomElement($array = array('à changer', 'endommagé', 'correct', 'neuf')));
+            $maintenance->setStatus($faker->randomElement($array = array ('à changer','endommagé','correct','neuf')));
             $maintenance->setMaintenanceDate($faker->dateTimeBetween($startDate = '- 2 years', $endDate = 'now', $timezone = null));
-            $maintenance->setemployee($employeeRep->find($faker->numberBetween($min = 101, $max = 200)));
-            $maintenance->setDocument($docRep->find($faker->numberBetween($min = 1, $max = 500)));
+            $maintenance->setemployee($employeeRep->find($faker->numberBetween($min = 100, $max = 200)));
+            $maintenance->setDocument($docRep->find($faker->numberBetween($min = 0, $max = 500)));
             $manager->persist($maintenance);
         }
         $manager->flush();
         
     }
 }
+
