ecde56b0826f690fa9f8eb4d493043cde6776070
Matthias-dmt <matthiasdumont.pro@gmail.com>
2020-07-21 14:57:14 +0200

    modification requête sql list of members registered last month. centralisation controlleur admin

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -5,9 +5,18 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Author;
+use App\Repository\MemberRepository;
+
 
 class AdminController extends AbstractController
 {
+
+    private $memberRep;
+
+    public function __construct(MemberRepository $memberRep)
+    {
+        $this->memberRep = $memberRep;
+    }
     /**
      * @Route("/admin", name="admin_index")
      */
@@ -24,6 +33,21 @@ class AdminController extends AbstractController
 
         return $this->render('admin/findMostAuthorsInCatalog.html.twig', [
             'authors' => $authors
+        ]);
+    }
+
+    public function listMemberRegLastMonth()
+    {
+        $month = new \DateInterval('P1M');
+        $date = new \DateTime();
+        $currentDate = $date->format('Y-m-d H:i:s');
+        $oneMonthBefore = $date->sub($month);
+
+        $listMemberRegLastMonth = $this->memberRep->listMemberRegisteredLastMonth($currentDate, $oneMonthBefore);
+        
+        
+        return $this->render('admin/listMemberRegLastMonth.html.twig', [
+            'listMemberRegLastMonth' => $listMemberRegLastMonth,
         ]);
     }
 }
