cf7d10ae6aa4d14ecccdf2dc417fc93f9071875a
Lucas-Alarcon <lucas.riuk@gmail.com>
2020-07-23 10:08:03 +0200

    affichage de l'email pour les usagers

--- src/Form/MemberType.php
+++ src/Form/MemberType.php
@@ -14,6 +14,7 @@ class MemberType extends AbstractType
         $builder
             ->add('pseudo')
             ->add('password')
+            ->add('email')
             ->add('firstName')
             ->add('lastName')
             ->add('postalCode')
